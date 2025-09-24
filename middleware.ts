1d9e01853c6d8b4b0fe099ad2148a9e8191812f6
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-24 23:10:56 +0200

    fix middleware -> wip

--- middleware.ts
+++ middleware.ts
@@ -2,28 +2,15 @@ import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
 
 export default withAuth(
 	async function middleware(req: any) {
-		// Add any custom middleware logic here
-		// console.log('look at me', req.kindeAuth);
+		console.log('look at me', req.kindeAuth);
 	},
 	{
-		// publicPaths: ['/home', '/dashboard', '/login'],
-		// Add proper error handling
-		onError: (error: Error) => {
-			console.error('Middleware error:', error);
-		},
+		publicPaths: ['/home', '/dashboard', '/login'],
 	},
 );
 
-// export const config = {
-// 	matcher: [
-// 		/*
-// 		 * Match all request paths except for the ones starting with:
-// 		 * - api (API routes)
-// 		 * - _next/static (static files)
-// 		 * - _next/image (image optimization files)
-// 		 * - favicon.ico (favicon file)
-// 		 * - public folder files
-// 		 */
-// 		'/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-// 	],
-// };
+export const config = {
+	matcher: [
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+	],
+};
