7bd5d06237289c8bd2f2c158cb2a6dd1efebefc2
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-24 21:32:55 +0200

    Add Kinde auth cheat sheet .md file -> wip

--- app/(users)/dashboard/page.tsx
+++ app/(users)/dashboard/page.tsx
@@ -1,8 +1,15 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 export default async function DashboardPage() {
-	const { getUser } = getKindeServerSession();
+	const { getUser, getAccessToken, getIdToken } = getKindeServerSession();
 	const user = await getUser();
+	console.log('[user]', user);
+
+	const accessToken = await getAccessToken();
+	console.log('[accessToken]', accessToken);
+
+	const idToken = await getIdToken();
+	console.log('[idToken]', idToken);
 
 	return <div>Welcome to the Dashboard user: {user?.username}</div>;
 }
