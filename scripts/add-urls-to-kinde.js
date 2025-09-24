341e4429fb708bfef623baf59e8b904a91e87320
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-24 23:45:40 +0200

    fix auth for deployed site -> wip

--- scripts/add-urls-to-kinde.js
+++ scripts/add-urls-to-kinde.js
@@ -1,3 +1,10 @@
+import { NextResponse } from 'next/server';
+import { jwt } from 'jsonwebtoken';
+import { usersTable } from '@/db/schema';
+import { db } from '@/db';
+import jwksClient from 'jwks-rsa';
+import { Env } from '@/env';
+
 async function getAuthToken() {
 	try {
 		const response = await fetch(`${process.env.KINDE_ISSUER_URL}/oauth2/token`, {
@@ -98,3 +105,77 @@ async function addCallbackUrlToKinde(token) {
 		}
 	}
 })();
+
+// The Kinde issuer URL should already be in your `.env` file
+// from when you initially set up Kinde. This will fetch your
+// public JSON web keys file
+const client = jwksClient({
+	jwksUri: `${Env.get('KINDE_ISSUER_URL')}/.well-known/jwks.json`,
+});
+
+export async function POST(req) {
+	try {
+		// Get the token from the request
+		const token = await req.text();
+
+		// Decode the token
+		const jwtDecoded = jwt.decode(token, { complete: true });
+
+		// Verify the token
+		if (!jwtDecoded) {
+			return NextResponse.json({ message: 'Invalid token: error decoding token' }, { status: 400 });
+		}
+
+		const header = jwtDecoded.header;
+		const kid = header.kid;
+		const key = await client.getSigningKey(kid);
+		const signingKey = key.getPublicKey();
+		const event = jwt.verify(token, signingKey);
+
+		// ! todo: create crud functions in a different file and import them here
+		switch (event?.type) {
+			case 'user.updated':
+				break;
+			case 'user.created':
+				try {
+					const user = event.data.user;
+					if (!user) {
+						return NextResponse.json({ message: 'Invalid user data' }, { status: 400 });
+					}
+					const database = await db;
+					console.log('[database] ', database);
+					const newUser = await database.insert(usersTable).values({
+						kinde_id: user.id,
+						email: user.email,
+						first_name: user.first_name,
+						last_name: user.last_name,
+						username: user.username,
+						organizations: user.organizations,
+						phone: user.phone,
+						is_password_reset_requested: user.is_password_reset_requested,
+						is_suspended: user.is_suspended,
+						role: 'jedi', // ! Default role - users can update this later in the app
+						created_at: new Date(),
+						updated_at: new Date(),
+					});
+					if (!newUser) {
+						return NextResponse.json({ message: 'Failed to create user' }, { status: 400 });
+					}
+					return NextResponse.json({ message: 'User created' }, { status: 200 });
+				} catch (err) {
+					console.error(err);
+				}
+				break;
+			case 'user.deleted':
+				break;
+			default:
+				break;
+		}
+	} catch (err) {
+		if (err instanceof Error) {
+			console.error(err.message);
+			return NextResponse.json({ message: err.message }, { status: 400 });
+		}
+	}
+	return NextResponse.json({ status: 200, statusText: 'success' });
+}
