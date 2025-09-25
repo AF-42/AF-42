5afc016d1fc9f87dc442b4dc40d56e36e6c771ae
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-25 19:28:15 +0200

    clean xo linting

--- app/(users)/dashboard/page.tsx
+++ app/(users)/dashboard/page.tsx
@@ -9,14 +9,14 @@ export default async function DashboardPage() {
 	const user = await getUser();
 
 	if (!(await isAuthenticated())) {
-		redirect('/home');
+	redirect('/home');
 	}
 
 	return (
-		<>
+	<>
 			<div>Welcome to the Dashboard user: {user?.username}</div>
 
-			<PortalLink>
+		<PortalLink>
 				<Button className="bg-cyan-600 hover:bg-cyan-700 text-white border-2 border-cyan-600 hover:border-cyan-700 transition-all duration-300 transform hover:scale-105 text-xs lg:text-sm">
 					Portal Link Name Dashboard
 				</Button>
