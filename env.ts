ad5434498fd240814776951b61d12de7b241c824
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-26 12:04:18 +0200

    update env.ts file

--- env.ts
+++ env.ts
@@ -16,6 +16,12 @@ const publicEnv: Record<string, string> = {
 	TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL as string,
 	TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN as string,
 	KINDE_ISSUER_URL: process.env.KINDE_ISSUER_URL as string,
+	KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID as string,
+	KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET as string,
+	KINDE_POST_LOGIN_REDIRECT_URL: process.env.KINDE_POST_LOGIN_REDIRECT_URL as string,
+	KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL as string,
+	KINDE_SITE_URL: process.env.KINDE_SITE_URL as string,
+	OPENAI_API_KEY: process.env.OPENAI_API_KEY as string,
 };
 
 export type EnvType = z.infer<typeof envSchema>;
