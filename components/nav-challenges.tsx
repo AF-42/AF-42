c93688afe352bf681c8f3a4d0923c146f4c91920
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-26 23:57:49 +0200

    challenges navbar component

---
+++ components/nav-challenges.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { Folder, MoreHorizontal, Share, Trash2, type LucideIcon } from 'lucide-react';
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+	SidebarGroup,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuAction,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	useSidebar,
+} from '@/components/ui/sidebar';
+
+export function NavChallenges({
+	challenges,
+}: {
+	challenges: {
+		name: string;
+		url: string;
+		icon: LucideIcon;
+	}[];
+}) {
+	const { isMobile } = useSidebar();
+
+	return (
+		<SidebarGroup className="group-data-[collapsible=icon]:hidden">
+			<SidebarGroupLabel>My Challenges</SidebarGroupLabel>
+			<SidebarMenu>
+				{challenges.map((item) => (
+					<SidebarMenuItem key={item.name}>
+						<SidebarMenuButton asChild>
+							<a href={item.url}>
+								<item.icon />
+								<span>{item.name}</span>
+							</a>
+						</SidebarMenuButton>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<SidebarMenuAction showOnHover>
+									<MoreHorizontal />
+									<span className="sr-only">More</span>
+								</SidebarMenuAction>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent
+								className="w-48"
+								side={isMobile ? 'bottom' : 'right'}
+								align={isMobile ? 'end' : 'start'}
+							>
+								<DropdownMenuItem>
+									<Folder className="text-muted-foreground" />
+									<span>View Challenges</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<Share className="text-muted-foreground" />
+									<span>Ongoing Challenges</span>
+								</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem>
+									<Trash2 className="text-muted-foreground" />
+									<span>Completed Challenges</span>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</SidebarMenuItem>
+				))}
+				<SidebarMenuItem>
+					<SidebarMenuButton>
+						<MoreHorizontal />
+						<span>More</span>
+					</SidebarMenuButton>
+				</SidebarMenuItem>
+			</SidebarMenu>
+		</SidebarGroup>
+	);
+}
