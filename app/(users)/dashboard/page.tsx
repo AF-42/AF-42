2da5a4e9cc3279abfc2882641742c0fb5c0571cc
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-26 14:06:34 +0200

    WIP

--- app/(users)/dashboard/page.tsx
+++ app/(users)/dashboard/page.tsx
@@ -1,9 +1,6 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
 
-import { PortalLink } from '@kinde-oss/kinde-auth-nextjs/components';
-import { Button } from '@/components/ui/button';
-
 export default async function DashboardPage() {
 	const { getUser, isAuthenticated } = getKindeServerSession();
 	const user = await getUser();
@@ -14,12 +11,7 @@ export default async function DashboardPage() {
 
 	return (
 		<>
-			<div>Welcome to the Dashboard user: {user?.username}</div>
-			<PortalLink>
-				<Button className="bg-cyan-600 hover:bg-cyan-700 text-white border-2 border-cyan-600 hover:border-cyan-700 transition-all duration-300 transform hover:scale-105 text-xs lg:text-sm">
-					Portal Link Name Dashboard
-				</Button>
-			</PortalLink>
+			<div className="text-2xl font-bold">Welcome to the Dashboard user: {user?.username}</div>
 		</>
 	);
 }
--- app/(users)/layout.tsx
+++ app/(users)/layout.tsx
@@ -1,10 +1,5 @@
-import MainLayout from '@/components/layouts/main-layout';
 import { AuthProvider } from '../AuthProvider';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<AuthProvider>
-			<MainLayout>{children}</MainLayout>
-		</AuthProvider>
-	);
+	return <AuthProvider>{children}</AuthProvider>;
 }
