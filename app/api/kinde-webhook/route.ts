638763ee189573bb34c5d18f2fda5c3dbd6e8d4c
BASIL ALBADER <basilalbapro@gmail.com>
2025-09-29 13:25:10 +0200

    update Kind-webhook from switch to if/else

--- app/api/kinde-webhook/route.ts
+++ app/api/kinde-webhook/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from 'next/server';
 import type { JwtPayload } from 'jsonwebtoken';
-import { usersTable } from '@/db/schema';
-import { db } from '@/db';
-import jwksClient from 'jwks-rsa';
-import jwt from 'jsonwebtoken';
 import { Env } from '@/env';
 import { usersService } from '@/services/users/users.service';
+import jwksClient from 'jwks-rsa';
+import jwt from 'jsonwebtoken';
 
 // The Kinde issuer URL should already be in your `.env` file
 // from when you initially set up Kinde. This will fetch your
@@ -33,42 +31,33 @@ export async function POST(req: Request) {
 		const signingKey = key.getPublicKey();
 		const event = jwt.verify(token, signingKey) as JwtPayload;
 
-		// ! todo: create crud functions in a different file and import them here
-		switch (event?.type) {
-			case 'user.updated':
-				break;
-			case 'user.created':
-				try {
-					const user = event.data.user;
-					if (!user) {
-						return NextResponse.json({ message: 'Invalid user data' }, { status: 400 });
-					}
-					const newUser = await usersService.createUser({
-						kinde_id: user.id,
-						email: user.email,
-						first_name: user.first_name,
-						last_name: user.last_name,
-						username: user.username,
-						organizations: user.organizations,
-						phone: user.phone,
-						is_password_reset_requested: user.is_password_reset_requested,
-						is_suspended: user.is_suspended,
-						role: '', // ! Default role - users can update this later in the app
-						created_at: new Date(),
-						updated_at: new Date(),
-					});
-					if (!newUser) {
-						return NextResponse.json({ message: 'Failed to create user' }, { status: 400 });
-					}
-					return NextResponse.json({ message: 'User created' }, { status: 200 });
-				} catch (err) {
-					console.error(err);
+		if (event?.type === 'user.created') {
+			try {
+				const user = event.data.user;
+				if (!user) {
+					return NextResponse.json({ message: 'Invalid user data' }, { status: 400 });
 				}
-				break;
-			case 'user.deleted':
-				break;
-			default:
-				break;
+				const newUser = await usersService.createUser({
+					kinde_id: user.id,
+					email: user.email,
+					first_name: user.first_name,
+					last_name: user.last_name,
+					username: user.username,
+					organizations: user.organizations,
+					phone: user.phone,
+					is_password_reset_requested: user.is_password_reset_requested,
+					is_suspended: user.is_suspended,
+					role: '',
+					created_at: new Date(),
+					updated_at: new Date(),
+				});
+				if (!newUser) {
+					return NextResponse.json({ message: 'Failed to create user' }, { status: 400 });
+				}
+				return NextResponse.json({ message: 'User created' }, { status: 200 });
+			} catch (err) {
+				console.error(err);
+			}
 		}
 	} catch (err) {
 		if (err instanceof Error) {
