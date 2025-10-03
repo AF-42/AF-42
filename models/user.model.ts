a26b86b72b4809e4429afdac7a5eccd6388167d8
BASIL ALBADER <basilalbapro@gmail.com>
2025-10-03 08:14:41 +0200

    wip -> user models

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,7 +1,12 @@
+import { usersTable } from '@/db/schema/users';
 import { api } from '@/lib/api-client';
 
 // Users model: this is used to interact with the users api endpoint in order to handle all fetching
 
 export const getUserProfile = async () => {
 	return api.get('users/profile').json();
+};
+
+export const updateUserProfile = async (user: typeof usersTable.$inferInsert) => {
+	return api.put('users/profile', { json: user }).json();
 };
